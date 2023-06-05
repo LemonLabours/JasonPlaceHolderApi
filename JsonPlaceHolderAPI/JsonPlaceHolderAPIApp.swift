6da02f8f93bf4a2d3494e2a163df3393843e077a
Lemon <101458752+LemonLabours@users.noreply.github.com>
2023-06-05 03:28:54 +0300

    Initial Commit

---
+++ JsonPlaceHolderAPI/JsonPlaceHolderAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonPlaceHolderAPIApp.swift
+//  JsonPlaceHolderAPI
+//
+//  Created by Lama AL Yousef on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct JsonPlaceHolderAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
